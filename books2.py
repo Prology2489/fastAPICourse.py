895003f35d80cd3e0647ee8f8117aa72d4a409d7
Prology2489 <hedgesdominic@yahoo.co.uk>
2024-11-18 13:51:19 +0000

    added search by publish date function

--- books2.py
+++ books2.py
@@ -10,13 +10,15 @@ class Book:    #Represents a book with attributes
     author: str
     description: str
     rating: int
+    published_date: int
     
-    def __init__(self, id, title, author, description, rating): #Initialization
+    def __init__(self, id, title, author, description, rating, published_date ): #Initialization
         self.id = id
         self.title = title
         self.author = author
         self.description = description
         self.rating = rating
+        self.published_date = published_date
 
 class BookRequest(BaseModel): #allows validation and converts data that can be stored
     id: Optional[int] = Field(description="ID is not needed on create", default=None)
@@ -24,6 +26,7 @@ class BookRequest(BaseModel): #allows validation and converts data that can be s
     author: str = Field(min_length=1)
     description: str =Field(min_length=1, max_length=100)
     rating: int = Field(gt=-1, lt=11)
+    published_date: int = Field(gt=1980, lt=2025)
 
     model_config = {   #this gives an outlay for people adding a new book
         "json_schema_extra":{
@@ -31,7 +34,8 @@ class BookRequest(BaseModel): #allows validation and converts data that can be s
                 "title": "author of the book",
                 "author": "Dune the chosen one",
                 "description": "A new description of a book",
-                "rating": 9
+                "rating": 9,
+                "published_date": 2012,
             }
         }
         }
@@ -39,12 +43,12 @@ class BookRequest(BaseModel): #allows validation and converts data that can be s
 
 
 BOOKS = [        # A list of Book objects to simulate a database
-    Book(1, "Lord Of The rings", "jrr tolkien", "a grand adventure", 10),
-    Book(2, "Star Wars", "georgre lucas", "epic space opera", 9),
-    Book(3, "Harry Potter", "jk rowling", "magical world", 7),
-    Book(4, "twilight", "stephenie meyer", "romance fantasy", 8),
-    Book(5, "Pacific Rim", "del toro", "war against monsters", 1),
-    Book(6, "Blade Runner", "denis villeneuve", "neo noir science fiction", 3)    
+    Book(1, "Lord Of The rings", "jrr tolkien", "a grand adventure", 10, 2002),
+    Book(2, "Star Wars", "georgre lucas", "epic space opera", 9, 1998),
+    Book(3, "Harry Potter", "jk rowling", "magical world", 7, 2003),
+    Book(4, "twilight", "stephenie meyer", "romance fantasy", 8, 2010),
+    Book(5, "Pacific Rim", "del toro", "war against monsters", 1, 2008),
+    Book(6, "Blade Runner", "denis villeneuve", "neo noir science fiction", 3, 2022)    
 ]
 
 @app.get("/books") #Fetch all books from the BOOKS list
@@ -66,7 +70,13 @@ async def read_book_by_rating(book_rating: int):
             books_to_return.append(book)
             return books_to_return
 
-
+@app.get("/books/publish/")   #filter books by published date
+async def read_book_by_published_date(published_date: int):
+    books_to_return = []
+    for book in BOOKS:
+        if book.published_date == published_date:
+            books_to_return.append(book)
+    return books_to_return
 
 @app.post("/create-book") #Add a new book to the BOOKS list.
 async def create_book(book_request: BookRequest):    #BookRequest: Ensures API input validation.
@@ -80,7 +90,19 @@ def find_book_id(book: Book):  #The function assigns an id dynamically based on 
     return book
 
 @app.put("/books/update_book")   # this function will allow us to update a book.
-async def Update_book(book: BookRequest):
+async def Update_book(book: BookRequest): #loop through the books to update the book i want
     for i in range(len(BOOKS)):
         if BOOKS[i].id == book.id:
             BOOKS[i] = book
+
+
+@app.delete("/books/[book_id]")
+async def delete_book(book_id: int): #delete book function
+    for i in range(len(BOOKS)): #loop through all books that match the ID then we can delete that item.
+        if BOOKS[i].id == book_id:
+            BOOKS.pop(i)
+            break
+
+
+
+
