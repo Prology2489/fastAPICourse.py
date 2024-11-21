f9b693e5f6c04e9653cfbafa5bb7a07b5f9c656b
Prology2489 <hedgesdominic@yahoo.co.uk>
2024-11-21 19:45:08 +0000

    added new class for users

--- ToDoApp/database.py
+++ ToDoApp/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 # Corrected database URL
-SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./todosapp.db"
 
 # Create the database engine
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
--- ToDoApp/models.py
+++ ToDoApp/models.py
@@ -1,12 +1,27 @@
 from database import Base
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+
+class Users(Base):
+   __tablename__ = "users"
+   
+   id = Column(Integer, primary_key=True, index=True)
+   email = Column(String, unique=True)
+   username = Column(String, unique=True)
+   first_name = Column(String)
+   last_name = Column(String)
+   hashed_password = Column(String)
+   is_active = Column(Boolean, default=True) # will show if a user is active
+   role = Column(String)
+
+   
 
 class Todos(Base): # new table in database called todos
-    __tablename__ = "todos"  #a way for sql to what to name the table inside the database
+    __tablename__ = "todos"  # a way for sql to what to name the table inside the database
 
     # will contain all these columns
-    id = Column(Integer, primary_key=True, index=True) #this id will be a new colum and will be a integar, index is a way to increase performance, this will be unique
+    id = Column(Integer, primary_key=True, index=True) # this id will be a new colum and will be a integar, index is a way to increase performance, this will be unique
     title = Column(String)
     description = Column(String)
     Priority = Column(Integer)
     complete = Column(Boolean, default=False)
+    owner_id = Column(Integer, ForeignKey("users.id")) # primary key of the user, so when a user signs ID we can match to primary key and find there to do list
