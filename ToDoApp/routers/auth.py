346db8bef43f46064cea675043bcb80cb57a4f43
Prology2489 <hedgesdominic@yahoo.co.uk>
2024-11-25 15:38:00 +0000

    added user authentication

--- ToDoApp/routers/auth.py
+++ ToDoApp/routers/auth.py
@@ -7,6 +7,9 @@ from passlib.context import CryptContext
 from database import SessionLocal
 from sqlalchemy.orm import Session
 from starlette import status
+from fastapi.security import OAuth2PasswordRequestForm
+
+
 
 
 
@@ -34,7 +37,16 @@ def get_db():
 db_dependency = Annotated[Session, Depends(get_db)]
 
 
-@router.post("/auth", status_code=status.HTTP_201_CREATED)
+def authenticate_user(username: str, password: str, db):  # new function that takes in username, password and database session
+    user = db.query(Users).filter(Users.username == username).first() # we query the username, qunique value
+    if not user:    # if nothing from database we rreturn false
+        return False
+    if not bcrypt_context.verify(password, user.hashed_password): # if we run and it takes in password and checks hash password and see if it macthes, tell us if user password is correct
+        return False # return false if no user matches
+    return True # return True if matches
+
+
+@router.post("/auth", status_code=status.HTTP_201_CREATED)  #created new users and saved it to our database
 async def create_user(db: db_dependency,
                       create_user_request: CreateUserRequest):
     create_user_model = Users(
@@ -51,7 +63,14 @@ async def create_user(db: db_dependency,
     db.commit()
 
 
+@router.post("/token") #token return to user and contains all information inside
 
+async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()],   # this function will validate the password and username and match to the database.
+                                 db: db_dependency):
+    user = authenticate_user(form_data.username, form_data.password, db)
+    if not user:
+        return 'Failed Authentication' # this will fail if user does not work
+    return 'Successful Authentication' # we pass in correct username and password              
 
 
 
